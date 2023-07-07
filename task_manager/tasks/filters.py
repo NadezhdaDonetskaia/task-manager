6cfb4c6bdee0b5a5ffbbc2a02dbc97e5256dbbc2
Nella <nella611@yandex.ru>
2023-07-07 17:52:32 +0300

    changed in tasks.filter name of label

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -1,15 +1,16 @@
 from django import forms
 from django_filters import FilterSet, AllValuesFilter
 from django_filters import ModelChoiceFilter, BooleanFilter
+from django.utils.translation import gettext
 from .models import Task, Label, User, Status
 from task_manager.logger_config import logger
 
 
 class TaskFilter(FilterSet, AllValuesFilter):
 
-    status = ModelChoiceFilter(field_name='status', queryset=Status.objects.all(), label='Status')
-    executor = ModelChoiceFilter(field_name='executor', queryset=User.objects.all(), label='Executor')
-    label = ModelChoiceFilter(field_name='label', queryset=Label.objects.all(), label='Label')
+    status = ModelChoiceFilter(field_name='status', queryset=Status.objects.all(), label=gettext('Статус'))
+    executor = ModelChoiceFilter(field_name='executor', queryset=User.objects.all(), label=gettext('Исполнитель'))
+    label = ModelChoiceFilter(field_name='label', queryset=Label.objects.all(), label=gettext('Метка'))
     my_tasks = BooleanFilter(field_name='my_tasks', method='filter_my_tasks', label='Только свои задачи', widget=forms.CheckboxInput())
 
     class Meta:
