013a5178aa71ce8339dc1e9b294b9fc9521eb778
Nella <nella611@yandex.ru>
2023-07-10 18:42:01 +0300

    changes filtr

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -10,12 +10,12 @@ class TaskFilter(FilterSet, AllValuesFilter):
 
     status = ModelChoiceFilter(field_name='status', queryset=Status.objects.all(), label=gettext('Статус'))
     executor = ModelChoiceFilter(field_name='executor', queryset=User.objects.all(), label=gettext('Исполнитель'))
-    label = ModelMultipleChoiceFilter(field_name='label', queryset=Label.objects.all(), label=gettext('Метка'))
+    labels = ModelMultipleChoiceFilter(field_name='labels', queryset=Label.objects.all(), label=gettext('Метка'))
     my_tasks = BooleanFilter(field_name='my_tasks', method='filter_my_tasks', label='Только свои задачи', widget=forms.CheckboxInput())
 
     class Meta:
         model = Task
-        fields = ['status', 'executor', 'label']
+        fields = ['status', 'executor', 'labels']
 
     def filter_my_tasks(self, queryset, name, value):
         if value:
